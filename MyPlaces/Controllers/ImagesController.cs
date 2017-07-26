ce6d9d89305153552583f30b34b9299832a2d3f0
Praneel Misthry <pmisthry@gmail.com>
2017-07-26 15:03:29 +0200

    Managed to filter some images

--- MyPlaces/Controllers/ImagesController.cs
+++ MyPlaces/Controllers/ImagesController.cs
@@ -11,12 +11,13 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using MyPlaces.Models;
 using FlickrNet;
+using System.Web;
 
 using System.Drawing;
 
 //For catching the JSON image data
 using System.Runtime.Serialization.Json;
-
+using System.IO;
 
 namespace MyPlaces.Controllers
 {
@@ -52,6 +53,31 @@ namespace MyPlaces.Controllers
             return Ok(images);
         }
 
+        //Local service function to convert images to byte arrays before saving to our database
+        //Pass in the large URL to set the image data
+        public byte[] ImageToArray(string photoUrl)
+        {
+            WebClient web = new WebClient();
+
+            //Download the image from its URL to the server
+            byte[] arr = web.DownloadData(photoUrl);
+
+            return arr;
+        }
+
+        //Use this to return a default image if no image data found on flickr
+        public byte[] ImageToArrayDefault()
+        {
+            Image img = Image.FromFile(@"C:\Batman-Jim-Lee.jpg");
+            byte[] arr;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                arr = ms.ToArray();
+            }
+            return arr;
+        }
+
         // GET: api/Images/5
         [ResponseType(typeof(Images))]
         [Route("{name}")]
@@ -61,11 +87,56 @@ namespace MyPlaces.Controllers
             //Get the first contact in the contacts list with the specified id
             //Pass in the location name and check if the main image description contains it
 
+            //Create the flickr objects and set it up on each call
+
+            
+            Flickr flickr = new Flickr();
+            flickr.ApiKey = "dba11127902261afd54826b290ed3de6";
+            flickr.ApiSecret = "1af450a5e13b378c";
+
+            //Create the byte arrays to store the binary data of each object returned through the JSON
+           
+            //Set up search options object
+            var options = new PhotoSearchOptions() { Tags = name, PerPage = 3, Page = 1, Extras = PhotoSearchExtras.LargeUrl | PhotoSearchExtras.Tags };
+          
             //This return all image objects including main description, landmark id and secondary images
             Images[] ImageArray = db.Images.Where<Images>(c => c.Description.Contains(name)).ToArray();
 
             //If the location or any duplicate of it is not found in the ImageArray the array size count will be O
+            //To make it easier convert this array to a list object for now
+            List<Images> myImageList = ImageArray.ToList<Images>();
 
+            //If we have 0 entries in the list corresponding to the search name then check flickr
+            int i = 0;
+            Images myImageObject = new Images();
+
+            if(myImageList.Count <= 0)
+            {
+                //Search for photos using the search option
+                //Search tags are set for a max of 3 photos per page
+                PhotoCollection photos = flickr.PhotosSearch(options);
+
+                //We should at least return 3 photos based on the tag
+
+                //If we found some photos between 1 and 3
+                if(photos.Count > 0 && photos.Count <= 3)
+                {
+
+                    //Only taking the first photos description due to versioning of the db
+                    myImageObject.Description = photos[0].Description;
+                    myImageObject.Image1 = ImageToArray(photos[0].LargeUrl);
+                    myImageObject.Image2 = ImageToArray(photos[1].LargeUrl);
+                    myImageObject.Image3 = ImageToArray(photos[2].LargeUrl);
+             
+                    //Add this to our list
+                    myImageList.Add(myImageObject);
+
+                    //Save these to our db for future purposes
+                    ImageArray = myImageList.ToArray<Images>();
+                }
+                
+            }
+   
             return ImageArray;
         }
 
